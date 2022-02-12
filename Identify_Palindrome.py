579f848e8cfe4b99ff5a4c8058953208c03de133
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-02-12 16:53:02 +0800

    Create Identify_Palindrome.py
    check if a word is a palindrome

---
+++ Identify_Palindrome.py
@@ -0,0 +1,33 @@
+
+# Write a function to determine if a given string is a palindrome
+# Palindrome - a word, phrase, or sequence 
+
+word = "madam"
+
+def check_palindrome_1(word):
+    word_list = []
+    for letter in word:
+        word_list.append(letter)
+
+    reverse =list(reversed(word_list))
+
+    if word_list == reverse:
+        print(f"{word} is a palindrome")
+    else:
+        print(f"{word} is not a palindrome")
+
+
+def check_palindrome_2(word):
+    reverse_word = ""
+    for number in range(1,len(word)+1):
+        print(number)
+        reverse_word += word[-(number)]
+    if word == reverse_word:
+        print(f"{word} is a palindrome")
+    else:
+        print(f"{word} is not a palindrome")
+    
+
+
+check_palindrome_2(word="level")
+
