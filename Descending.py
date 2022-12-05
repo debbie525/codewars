7a2484fe10c157861b23c6601f07a8db6c3c8461
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-12-05 11:23:29 +0800

    Update Descending.py
    Added 2nd solution without using the .sorted() function.

--- Descending.py
+++ Descending.py
@@ -5,24 +5,59 @@ Your task is to make a function that can take any non-negative integer
 as an argument and return it with its digits in descending order. 
 Essentially, rearrange the digits to create the highest possible number.
 
-
 '''
-#def descending(mynum):
-num = input('Please enter an integer:')
-print('Your number is: ' + num)
 
-num_list = []
-for item in num:
-    num_list.append(int(item))
-#print(num_list)
-descend_list=sorted(num_list, reverse=True)
+def sorted_integer(num):
 
-#print(descend_list)
+    num_list = []
+    for item in str(num):
+        num_list.append(int(item))
 
-final_output = ''
-for item in descend_list:
-    final_output += str(item)
-print ('Sorted in Descending Order: ' + final_output)
+    descend_list=sorted(num_list, reverse=True)
 
+    final_output = ''
+    for item in descend_list:
+        final_output += str(item)
+    return final_output
+
+print(sorted_integer(8899822754403519))
+
+#-------------------------------------------------------------
+
+def sorted_integer_2(num):
+    num_list = []
+
+    for element in str(num):
+        num_list.append(int(element))
+
+    descend_list =[num_list[0]]
+    num_list.remove(num_list[0])
+
+    for number in num_list:
+        if number in descend_list:
+            descend_list.insert(((descend_list.index(number))+1), number)
+
+        elif number<descend_list[0] and number>descend_list[-1]:
+            for item in reversed(descend_list) :
+                if number < item:
+                    if descend_list.count(item)>1:
+                        descend_list.insert((descend_list.index(item)+descend_list.count(item)), number)
+                    else:
+                        descend_list.insert((descend_list.index(item)+1), number)
+                    break
+
+        elif number > descend_list[0]:
+            descend_list.insert(0, number)
+            
+        elif number < descend_list[-1]:
+            descend_list.append(number)
+
+    final_output=""
+    for item in descend_list:
+        final_output+=str(item)
+
+    return final_output
+            
+print(sorted_integer_2(8899822754403519))   
 
 
