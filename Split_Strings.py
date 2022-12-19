88aa005896f7064a19ed58af85253510c2556460
debbie525 <57734103+debbie525@users.noreply.github.com>
2022-12-19 14:12:55 +0800

    Update Split_Strings.py
    update the solution

--- Split_Strings.py
+++ Split_Strings.py
@@ -1,33 +1,34 @@
-# Complete the solution so that it splits the string into pairs of two characters. 
+# Create a funtion that will split a string into pairs of two characters. 
 # If the string contains an odd number of characters then it should replace the missing 
 # second character of the final pair with an underscore ('_').
 
 # Examples:
-# 'abc' =>  ['ab', 'c_']
-# 'abcdef' => ['ab', 'cd', 'ef']
 
+#  'abc' =>  ['ab', 'c_']
+#  'abcdef' => ['ab', 'cd', 'ef']
 
-def split_string(input_string):
-    string_set =""
-    splitted_string =[]
-    count=0
-    for item in input_string:
-        count+=1
-        string_set+=item
-        if len(string_set)==2:
-            splitted_string.append(string_set)
-            string_set=""             # re-initialize the value
+def split_into_pair(string):
+    new_string = []
+    new_char =''
 
-        # if length of input string is odd and iteration at last item
-        if len(input_string)%2!=0 and count ==len(input_string):
-            string_set = input_string[-1] + "_"  # last character plus "_"
-            splitted_string.append(string_set)
+    for i in range(len(string)):
+        new_char+=string[i]
+        if len(new_char)==2:                    
+            new_string.append(new_char)         # append two characters in new array
+            new_char =''                        # reset/empty the new_char
 
-    return splitted_string
+    if len(string)%2!=0:                        # check if the length of string is odd                    
+        new_char+="_"                           # add "_" to the remaining 1 character
+        new_string.append(new_char)
+    
+    return new_string
 
-print(split_string("abc"))
-print(split_string("abcdef"))
-print(split_string("asdfadsf"))
-print(split_string("asdfads"))
-print(split_string(""))
+#--------------------------------------------------------------------
+print(split_into_pair("abc"))
+print(split_into_pair("abcdef"))
+print(split_into_pair("asdfadsf"))
+print(split_into_pair(""))
+print(split_into_pair("a"))
+
+        
 
